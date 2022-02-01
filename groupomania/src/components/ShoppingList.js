f1d4bfe6a60fc97fefa377a9539cfb56bb8bfa50
Marion Carraud <marion.cd@mailfence.com>
2022-02-01 16:51:08 +0100

    tableau unique de catégorie (exercice react)

--- groupomania/src/components/ShoppingList.js
+++ groupomania/src/components/ShoppingList.js
@@ -1,26 +1,32 @@
-import { plantList } from '../datas/plantList'
+import { plantList } from '../datas/plantList' // import de la liste de plante
 
-function ShoppingList() {
-	const categories = plantList.reduce(
-        (acc, plant) =>
-            acc.includes(plant.category) ? acc : acc.concat(plant.category), []
+function ShoppingList(){
+    const categories = plantList.reduce((acc, num) =>
+        acc.includes(num.category) ? acc : acc.concat(num.category), // opérateur ternaire
+        [] 
+
+        // l'accumulateur contient la catégorie ? 
+        // si oui l'accumulateur reste le même :
+        // sinon, l'accumulateur est fusionnée avec la catégorie
+        //  line 6 : un tableau vide pour ne pas créer d'erreur s'il n'y a rien dans plantList
     )
 
-    return (
-        <div>
-            <ul>
-                {categories.map((cat) => (
-                    <li key={cat}>{cat}</li>
-                ))}
-            </ul>
-            <ul>
-                {plantList.map((plant) => (
-                    <li key={plant.id}>{plant.name}</li>
-                ))}
-            </ul>
+    return <div>
+                <ul>
+                    {categories.map((cat) => (
+                        <li key={cat}>{cat}</li>
+                    ))}
+                
+                </ul>
 
-        </div>
-    )
-}
+                <ul>
+                    {plantList.map((plant) => (
+                        <li key={plant.id}>{plant.name}</li>
+                    ))}
+                
+                </ul>
+            </div>
+
+};
 
 export default ShoppingList
