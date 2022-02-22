68ae9cfc69509b1b2cbb137f72234e820437fcd2
Marion Carraud <marion.cd@mailfence.com>
2022-02-22 15:40:05 +0100

    refonte accueil connecté

--- frontend/src/components/Actus/Center/ActusCenter.js
+++ frontend/src/components/Actus/Center/ActusCenter.js
@@ -1,79 +1,62 @@
-import '../Actus.css'
-import { Link } from 'react-router-dom'
-import photoProfil from '../../../assets/Photo-profil-defaut.png'
+import '../Actus.css';
+import { Link } from 'react-router-dom';
+import photoProfil from '../../../assets/Photo-profil-defaut.png';
+import { useState } from 'react'
+import InputEmoji from 'react-input-emoji'
+
+import SendIcon from "@material-ui/icons/Send";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon"
+import CancelIcon from "@material-ui/icons/Cancel"
 
 function ActusCenter() {
+    const [ message, setMessage ] = useState('')
+  
+        const handleOnEnter = (message) => {
+          console.log('enter', message)
+        }
 
     return (
             <div className='accueil__center'>
+                <h2>Bienvenue sur Groupomania !</h2>
                 <div className="statut-a-publier blocBleu">
                     <div className="statut-a-publier__avatar-text">
-                        <img src={photoProfil} alt="photo profil" className="grp-banner__vignette-profil statut-left"/>
-                        <textarea type="text" placeholder="Que voulez-vous partager aujourd'hui ?" className="statut-center"></textarea>
-                        <div className="statut-right"></div>
-                    </div>
-                    <div className="statut-a-publier__icones">
-                        <div className="statut-left"></div>
-                        <div className="statut-center">
-                            <div className="statut-center__bloc1">Ajouter à votre publication : &nbsp;</div>
-                            <a href="#" className="ajout-photo"><div className="statut-center__bloc2">📸</div></a>
-                            <a href="#" className='ajout-photo'><div className="statut-center__bloc2">😃</div></a>
-                            <button className='statut__btn'>Publier</button>
-                        </div>
                         
-                        <div className="statut-right"></div>
-                    </div>
-                </div>
-
-                <h2>Ce que votre réseau a écrit</h2>
-
-                <div >
-
-                    <div className="statut-a-publier blocRouge">
-                        <div className="statut-a-publier__avatar-text">
-                            <div className="statut-left reseau__statut--profil">
-                                <img src={photoProfil} alt="photo profil" className="grp-banner__vignette-profil"/>
-                                <div className="reseau__statut--nom"><a href="#">Prénom&nbsp;Nom</a></div>
-                            </div>
-                            <div className="publications__texte statut-center">Globos latera urbis ait cuncta quod 
-                            per familiarium subversasque urbis terga subversasque 
-                            transiturus tamquam latera discurrunt etc. etc.
-                            </div>
+                            <img src={photoProfil} alt="photo profil" className="grp-banner__vignette-profil statut-left"/>
                             
+                            <InputEmoji 
+                            value={message}
+                            onChange={setMessage}
+                            cleanOnEnter
+                            // onEnter={handleOnEnter}
+                            fontFamily= "Cambria, Cochin, Georgia, Times, serif"
+                            placeholder="Que voulez-vous partager aujourd'hui ?"/>
+
                             <div className="statut-right"></div>
-                        </div>
+    
+                    </div>
 
                     <div className="statut-a-publier__icones">
-                        <div className="statut-left"></div>
-                        <div className="statut-center">
-                            <div className="statut-a-publier">
-                                <div className="statut-a-publier__avatar-text">
-                                    <img src={photoProfil} alt="photo profil" className="grp-banner__vignette-profil statut-left"/>
-                                    <textarea type="text" placeholder="Commenter..." className="statut-center"></textarea>
-                                    <div className="statut-right"></div>
-                                </div>
-                                
-                                <div className="statut-a-publier__icones">
-                                    <div className="statut-left"></div>
-                                    <div className="statut-center">  
-                                        <div className="statut-center__bloc1">Ajouter à votre publication : &nbsp;</div>
-                                        <a href="#" className='ajout-photo'><div className="statut-center__bloc2">📸 </div></a>
-                                        <a href="#" className='ajout-photo'><div className="statut-center__bloc2">😃</div></a>
-                                        <button className='commentaire__btn'>Commenter</button>
-                                    </div>
-                                    <div className="statut-right"></div>
-                                </div>
-                            </div>
-                            
-                        </div>
-                        <div className="statut-right"></div>
+
+                        
                     </div>
-                    
                 </div>
-                
-                    
+                {/* <div className="statut-a-publier blocBleu">
+                    <div className="statut-a-publier__avatar-text">
+                        <img src={photoProfil} alt="photo profil" className="grp-banner__vignette-profil statut-left"/>
+                        
+                        <InputEmoji
+                            value={message}
+                            onChange={setMessage}
+                            cleanOnEnter
+                            // onEnter={handleOnEnter}
+                            placeholder="Que voulez-vous partager aujourd'hui ?"/> */}
+
+                        {/* <textarea type="text" placeholder="Que voulez-vous partager aujourd'hui ?" className="statut-center" onChange={(e) => setContenu(e.target.value)}></textarea> */}
+                        {/* <div className="statut-right"></div>
+                    </div>
                 </div>
 
+                <h2>Ce que votre réseau a écrit</h2> */}
             </div>
     )
 }
