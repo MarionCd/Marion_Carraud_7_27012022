c6eecaad05ef0e2b4bc78c03818cc90ef73011e6
Marion Carraud <marion.cd@mailfence.com>
2022-02-23 21:51:27 +0100

    ajout smiley commentaires

--- frontend/src/components/Actus/Center/ActusCenter.js
+++ frontend/src/components/Actus/Center/ActusCenter.js
@@ -10,26 +10,44 @@ import '../Actus.css';
 import photoProfil from '../../../assets/Photo-profil-defaut.png'; //à modifier avec la photo dans la bdd
 
 function ActusCenter() {
+    const prenom = "Prénom"
+    const nom = "Nom"
+
     const [message, setMessage] = useState();
+    const [commentaire, setCommentaire] = useState();
     const [showPicker, setShowPicker] = useState(false);
+    const [showPickerComment, setShowPickerComment] = useState(false);
+
+    const auteur = prenom+" "+nom;
 
     const onEmojiClick = (e, emojiObject) => { // choix d'un smiley
-      setMessage(start => start + emojiObject.emoji); 
+      setMessage(prevInput => prevInput + emojiObject.emoji);
       setShowPicker(false);
     };
 
-    const handleChange = e =>{
+    const handleChange = (e) =>{
         setMessage(e.target.value);
-    }
-
-    const validate = () => { console.log(message) // récupération de contenu et envoi des données au backend avec axios
-        let post = {
-            id: 3,
-            contenu: message
-        } 
-        posts.push(post)   
-        setMessage()
     };
+
+    const onEmojiClickComment = (e, emojiObject) => { // choix d'un smiley
+        setCommentaire(start => start + emojiObject.emoji);
+        setShowPickerComment(false);
+    };
+
+    const handleChangeComment = (e) =>{
+        setCommentaire(e.target.value);
+    };
+
+    const validate = () => { 
+        let post = {
+            id: 1,
+            contenu: message,
+            author: auteur
+        }
+        posts.push(post)
+        setMessage()
+        document.getElementById('statut-publication').value = '';
+    }
 
     const [posts, setPosts] = useState([]);
   
@@ -40,12 +58,16 @@ function ActusCenter() {
 
                 <div className="statut-a-publier blocBleu">
                     <div className="statut-a-publier__avatar-text">
-                        <img src={photoProfil} alt="photo profil" className="grp-banner__vignette-profil statut-left"/>
-                        
+                        <div>
+                            <img src={photoProfil} alt="photo profil" className="grp-banner__vignette-profil statut-left"/>
+                            <div className="auteur">
+                                <span className="auteur-prenom" >{prenom}</span> <span className="auteur-nom" >{nom}</span>
+                            </div>
+                        </div>
                         <div className='statut-center publication'>
-                            <textarea type="text" placeholder="Que voulez-vous partager aujourd'hui ?" value={message} onChange={handleChange} ></textarea>  
+                            <textarea id="statut-publication" type="text" placeholder="Que voulez-vous partager aujourd'hui ?" value={message} onChange={handleChange} ></textarea>  
                             <div className="encart-btn">
-                                <button className="statut__btn" onClick={() => validate()}>Publier</button>
+                                <div className="statut__btn" onClick={() => validate()}>Publier</div>
                             </div>    
                         </div> 
 
@@ -55,8 +77,8 @@ function ActusCenter() {
                             </div>
 
                             <div className="parent__ajout-photo">
-                                    <button className="picture-icon">📸</button>
-                                    <input type="file" name="upfile" />
+                                    <button className="picture-icon" >📸</button>
+                                    <input type="file" name="upfile" accept=".jpeg,.jpg,.gif" />
                             </div>
                             {showPicker && <Picker className="emoji-list" onEmojiClick={onEmojiClick} />}
                           
@@ -81,26 +103,31 @@ function ActusCenter() {
                                 <div className="publi-ami" >{post.contenu}</div>  
                                 
                                 <div className="publi-ami__commentaire statut-a-publier__avatar-text">
-                                    <img src={photoProfil} alt="photo profil" className="grp-banner__vignette-profil statut-left"/>
-                    
+                                    <div>
+                                        <img src={photoProfil} alt="photo profil" className="grp-banner__vignette-profil statut-left"/>
+                                        <div className="auteur">
+                                            <span className="auteur-prenom">Prénom</span> <span className="auteur-nom">Nom</span>
+                                        </div>
+                                    </div>
                                     
                                     <div className="statut-center publication">
-                                        <textarea type="text" placeholder="Commenter la publication"  ></textarea>
+                                        <textarea type="text" placeholder="Commenter la publication" value={commentaire} onChange={handleChangeComment} ></textarea>
                                         <div className="encart-btn">
-                                            <button className="commentaire__btn" onClick={() => validate()}>Commenter</button>
+                                            <button className="commentaire__btn" >Commenter</button>
                                         </div>
                                     </div>
 
                                     <div className="statut-right bouton-publication">
                                         <div className="parent__ajout-photo">
-                                            <button className="picture-icon" >😃</button>
+                                            <button className="picture-icon" onClick={() => setShowPickerComment(val => !val)}>😃</button>
                                         </div>
 
                                         <div className="parent__ajout-photo">
                                             <button className="picture-icon">📸</button>
-                                            <input type="file" name="upfile" />
+                                            <input type="file" name="upfile" accept=".jpeg,.jpg,.gif" />
                                         </div>
 
+                                        {showPickerComment && <Picker className="emoji-list" onEmojiClick={onEmojiClickComment} />}
                                         {/* {showEmojis && <Emoji className="emoji-list" pickEmoji={pickEmoji} />} */}
                                     </div>
                                 </div>
