92af6a0613bfe12dabe15c9058d6949128e4b714
Marion Carraud <marion.cd@mailfence.com>
2022-02-25 13:09:16 +0100

    controller login

--- backend/controllers/signup.js
+++ backend/controllers/signup.js
@@ -1,8 +1,7 @@
 const bcrypt = require ('bcrypt');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const db = require('../db/db');
-
+// const db = require('../db/db');
 
 /***************** Inscription utilisateur ********* */
 exports.signup = async (req, res, next) => {
@@ -13,77 +12,7 @@ exports.signup = async (req, res, next) => {
     name: req.body.name,
     lastname: req.body.lastname
   })
-  .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+  .then(() => res.status(201).json({ success : 'Utilisateur créé !' }))
   .catch(error => res.status(400).json({ error }));
         
 };
-
-
-// /********** Inscription client ********/
-// exports.signup = (req, res, next) => {
-//     bcrypt.hash(req.body.password, 10)
-//       .then(hash => {
-//         const user = new User({
-//           email: req.body.email,
-//           password: hash
-//         });
-//         user.save()
-//           .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-//           .catch(error => res.status(400).json({ error }));
-//       })
-//       .catch(error => res.status(500).json({ error }));
-//   };
-
-// /***************** Inscription utilisateur ********* */
-// exports.signup = async (req, res, next) => {
-//   // const {body} = req
-  
-//   console.log("données", req.body)
-//     try {
-//     const hash = await bcrypt.hash(req.body.password, 10)
-          
-//     const newUser = await User.create({ 
-//       password: hash,
-//       name: req.body.name,
-//       lastname: req.body.lastname
-//     }) 
-//   return res.status(200).send({ success : "utilisateur créé" })
-//   } catch(error){console.log(error)}
-// };
-
-
-/***********ce que j'ai essayé */
-// exports.signup = async (req, res, next) => {
-//   const hash = await bcrypt.hash(req.body.password, 10)
-//         .then(hash => {
-//           const user = ({
-//             email : req.body.email,
-//             password: hash,
-//             name: req.body.name,
-//             lastname: req.body.lastname
-//           });
-//           User.create() 
-//             .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-//             .catch(error => res.status(400).json({ error }));
-//         })
-//         .catch(error => res.status(500).json({ error }));
-//     };
-
-/*
-exports.signup = async (req, res, next) => {
-  const hash = await bcrypt.hash(req.body.password, 10)
-        // .then(hash => {
-        //   const user = ({
-        //     email : req.body.email,
-        //     password: hash,
-        //     name: req.body.name,
-        //     lastname: req.body.lastname
-        //   });
-        //   User.create() 
-        //     .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        //     .catch(error => res.status(400).json({ error }));
-        // })
-        // .catch(error => res.status(500).json({ error }));
-};
-
-*/
